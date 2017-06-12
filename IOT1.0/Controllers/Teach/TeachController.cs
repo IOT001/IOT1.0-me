0f9e1387ddde414cf0973bf7dbaada6b624d4949
IOT001 <msx8852@126.com>
2017-06-12 17:28:26 +0800

    增加图片上传功能

--- IOT1.0/Controllers/Teach/TeachController.cs
+++ IOT1.0/Controllers/Teach/TeachController.cs
@@ -4,8 +4,10 @@ using DataProvider.Entities;
 using DataProvider.Models;
 using IOT1._0.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,12 +39,12 @@ namespace IOT1._0.Controllers.Teach
             model.SourceIL = CommonData.Instance.GetBropDownListData(SourceIL);
 
             model.Studentlist = StudentData.GetButtonList(search);//填充页面模型数据
-             return View(model);//返回页面模型
+            return View(model);//返回页面模型
 
 
         }
         /// <summary>
-        /// 根据按钮ID获取按钮详细信息
+        /// 根据按钮ID获取详细信息
         /// </summary>
         /// <param name="ID"></param>
         /// <returns></returns>
@@ -88,8 +90,8 @@ namespace IOT1._0.Controllers.Teach
                 return Json(ajax);
             }
 
-             
-          
+
+
 
 
             if (StudentData.UpdateStudent(Stu))//注意时间类型，而且需要在前台把所有的值
@@ -126,21 +128,21 @@ namespace IOT1._0.Controllers.Teach
                 ajax.msg = "手机号码已存在，不能重复使用！";
                 return Json(ajax);
             }
-               
+
             Stu.CreateTime = DateTime.Now;
             Stu.CreatorId = UserSession.userid;
 
             Students query = StudentData.GetStudentsList();//获取创建时间最新的一条数据
             string MAX_ID;
-            if (query !=null)
+            if (query != null)
             {
-                  MAX_ID = query.ID;
+                MAX_ID = query.ID;
             }
             else
             {
-                MAX_ID=null;
+                MAX_ID = null;
             }
-           
+
             var year = Stu.Birthday.ToString().Substring(2, 2);
 
             if (!string.IsNullOrEmpty(MAX_ID))
@@ -152,8 +154,8 @@ namespace IOT1._0.Controllers.Teach
                 Stu.ID = year + "0001";
             }
 
-              
-            if (StudentData.AddStudent(Stu)!="")//注意时间类型，而且需要在前台把所有的值
+
+            if (StudentData.AddStudent(Stu) != "")//注意时间类型，而且需要在前台把所有的值
             {
                 ajax.msg = "新增成功！";
                 ajax.status = EnumAjaxStatus.Success;
@@ -161,10 +163,42 @@ namespace IOT1._0.Controllers.Teach
             return Json(ajax);
         }
 
- 
 
 
- 
+
+
+
+
+        /// <summary>
+        /// 上传图片
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns> 
+        public JsonResult Upload()
+        {
+
+            AjaxStatusModel ajax = new AjaxStatusModel();//功能操作类的返回类型都是AjaxStatusModel，数据放到AjaxStatusModel.data中，前台获取json后加载
+            ajax.status = EnumAjaxStatus.Error;//默认失败
+            ajax.msg = "新增失败！";//前台获取，用于显示提示信息
+            var data = Request["data"];//获取前台传递的数据，主要序列化
+            if (string.IsNullOrEmpty(data))
+            {
+                return Json(ajax);
+            }
+            JObject jsonObj = JObject.Parse(data);
+            Picture Picture = new Picture();
+            var ret = Picture.DPUpLoadPhoto(jsonObj);              //.DPUpLoadPhotos(json);
+            ajax.msg = "上传成功！";
+            ajax.status = EnumAjaxStatus.Success;
+            ajax.data = ret;
+            return Json(ajax);
+
+        }
+
+
+
+
+
 
         public ActionResult Curriculum()
         {
@@ -175,100 +209,14 @@ namespace IOT1._0.Controllers.Teach
         {
             return View();
         }
- 
-
-
-        /// <summary>
-        /// 教师列表查询
-        /// </summary>
-        public ActionResult Teacher(TeacherSearchModel search)
+        public ActionResult Teacher()
         {
-            TeacherViewModel model = new TeacherViewModel();//页面模型
-            model.search = search;//页面的搜索模型
-            model.search.PageSize = 15;//每页显示
-            model.search.CurrentPage = Convert.ToInt32(Request["pageindex"]) <= 0 ? 1 : Convert.ToInt32(Request["pageindex"]);//当前页
-            //按钮下拉项
-            List<CommonEntity> ButtonIL = CommonData.GetDictionaryList(4);//1是字典类型值,仅供测试参考
-            model.buttonIL = CommonData.Instance.GetBropDownListData(ButtonIL);
-
-            model.buttonlist = TeacherData.GetButtonList(search);//填充页面模型数据
-            return View(model);//返回页面模型
+            return View();
         }
 
-        /// <summary>
-        /// 保存编辑按钮
-        /// </summary>
-        /// <param name="json"></param>
-        /// <returns></returns>
-        public JsonResult SaveButton()
-        {
-            AjaxStatusModel ajax = new AjaxStatusModel();//功能操作类的返回类型都是AjaxStatusModel，数据放到AjaxStatusModel.data中，前台获取json后加载
-            ajax.status = EnumAjaxStatus.Error;//默认失败
-            ajax.msg = "保存失败！";//前台获取，用于显示提示信息
-            var data = Request["data"];//获取前台传递的数据，主要序列化
-            if (string.IsNullOrEmpty(data))
-            {
-                return Json(ajax);
-            }
-            Teachers btn = (Teachers)(JsonConvert.DeserializeObject(data.ToString(), typeof(Teachers)));
-
-            if (TeacherData.UpdateTeacher(btn))//注意时间类型，而且需要在前台把所有的值
-            {
-                ajax.msg = "保存成功！";
-                ajax.status = EnumAjaxStatus.Success;
-            }
-            return Json(ajax);
-        }
-
-        /// <summary>
-        /// 新增教师
-        /// </summary>
-        /// <returns></returns>
-        public JsonResult AddTeach()
-        {
-            AjaxStatusModel ajax = new AjaxStatusModel();//功能操作类的返回类型都是AjaxStatusModel，数据放到AjaxStatusModel.data中，前台获取json后加载
-            ajax.status = EnumAjaxStatus.Error;//默认失败
-            ajax.msg = "新增失败！";//前台获取，用于显示提示信息
-            var data = Request["data"];//获取前台传递的数据，主要序列化
-            if (string.IsNullOrEmpty(data))
-            {
-                return Json(ajax);
-            }
-            Teachers btn = (Teachers)(JsonConvert.DeserializeObject(data.ToString(), typeof(Teachers)));
-
-            btn.CreateTime = DateTime.Now;
-            btn.CreatorId = UserSession.userid;
-            if (!string.IsNullOrEmpty(TeacherData.AddTeach(btn)))//注意时间类型，而且需要在前台把所有的值
-            {
-                ajax.msg = "新增成功！";
-                ajax.status = EnumAjaxStatus.Success;
-            }
-            return Json(ajax);
-        }
-
-
-        /// <summary>
-        /// 根据按钮ID获取按钮详细信息
-        /// </summary>
-        /// <param name="ID"></param>
-        /// <returns></returns>
-        public JsonResult GetTeachByID(string id)
-        {
-            AjaxStatusModel ajax = new AjaxStatusModel();//功能操作类的返回类型都是AjaxStatusModel，数据放到AjaxStatusModel.data中，前台获取json后加载
-            ajax.status = EnumAjaxStatus.Error;//默认失败
-            ajax.msg = "获取失败！";//前台获取，用于显示提示信息
-            Teachers btn = TeacherData.GetTeachByID(id);//业务层获取底层方法，返回数据
-            if (btn != null)
-            {
-                ajax.data = btn;//放入数据
-                ajax.msg = "获取成功！";
-            }
-            return Json(ajax);
-
-        }
     }
 }
- 
-  
- 
+
+
+
 
