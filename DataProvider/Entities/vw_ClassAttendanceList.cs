649b03c70047af9fb4611bd926dee548f2fe48bb
xiaowei <wei.xiao@525j.com.cn>
2017-06-14 17:54:37 +0800

    添加班级考勤实体类

---
+++ DataProvider/Entities/vw_ClassAttendanceList.cs
@@ -0,0 +1,143 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Entities
+{
+    /// <summary>
+    /// 课程考勤列表
+    /// </summary>
+    public class vw_ClassAttendanceList
+    {
+        /// <summary>
+        /// ClassID
+        /// </summary>
+        public   string ClassID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// ClassIndex
+        /// </summary>
+        public   int ClassIndex
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// ClassName
+        /// </summary>
+        public   string ClassName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// ClassDate
+        /// </summary>
+        public   DateTime ClassDate
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// TimePeriod
+        /// </summary>
+        public   string TimePeriod
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// StateID
+        /// </summary>
+        public   string StateID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// TeacherID
+        /// </summary>
+        public   string TeacherID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// RoomID
+        /// </summary>
+        public   int? RoomID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// TeacherName
+        /// </summary>
+        public   string TeacherName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// RoomName
+        /// </summary>
+        public   string RoomName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// SumStudents
+        /// </summary>
+        public   int? SumStudents
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// CheckINStudents
+        /// </summary>
+        public   int? CheckINStudents
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// UnCheckStudents
+        /// </summary>
+        public   int? UnCheckStudents
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// EvaluateStudents
+        /// </summary>
+        public   int? EvaluateStudents
+        {
+            get;
+            set;
+        }        
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class vw_ClassAttendanceListORMMapper : ClassMapper<vw_ClassAttendanceList>
+    {
+        public vw_ClassAttendanceListORMMapper()
+        {
+            base.Table("vw_ClassAttendanceList");
+
+            //Map(f => f.socketouts).Ignore();//设置忽略
+            //Map(f => f.ClassID).Key(KeyType.Assigned);//设置主键  (如果主键名称不包含字母“ID”，请设置)      
+            AutoMap();
+        }
+    }
+}
