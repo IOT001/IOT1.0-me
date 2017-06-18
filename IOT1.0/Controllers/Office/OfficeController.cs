3de800427e84d191079bdb0e7b310a509ce146a0
zhouruizr <287323540@qq.com>
2017-06-18 23:18:18 +0800

    zr

---
+++ IOT1.0/Controllers/Office/OfficeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOT1._0.Controllers.Office
+{
+    public class OfficeController : Controller
+    {
+        //
+        // GET: /Office/
+
+        public ActionResult MessageList()
+        {
+            return View();
+        }
+
+    }
+}
