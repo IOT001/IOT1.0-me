d3f43c6896235f12da7bf691f78ca315a86563d1
zhouruizr <287323540@qq.com>
2017-07-11 00:42:52 +0800

    留言查看

---
+++ IOT1.0/Controllers/WeiXin/MessageListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOT1._0.Controllers.WeiXin
+{
+    public partial class WeixinController : Controller
+    {
+        //
+        // GET: /MessageList/
+
+        public ActionResult MessageBrowse()
+        {
+            return View();
+        }
+
+    }
+}
