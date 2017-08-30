d3b1ab72886c4231310a5bd38ab92b8fd3a037e4
zhouruizr <287323540@qq.com>
2017-08-30 09:12:23 +0800

    12

--- DataProvider/Entities/EnrollAudit.cs
+++ DataProvider/Entities/EnrollAudit.cs
@@ -95,20 +95,12 @@ namespace DataProvider.Entities
         /// ApprovedRemark
         /// </summary>				
         public string ApprovedRemark { get; set; }
-<<<<<<< HEAD
+
         /// <summary>
         /// 收款记录，用逗号隔开，1现金，2pos，3微信，4支付宝，5扣卡
         /// </summary>
         public string CollectionRec { get; set; }
-=======
 
-
-        /// <summary>
-        /// 1现金，2pos，3微信，4支付宝，5扣卡
-        /// </summary>				
-        public string CollectionRec { get; set; }
-
->>>>>>> 7864357bf3528c2ef51b4c4892c5873becbaaa93
     }
 
 
