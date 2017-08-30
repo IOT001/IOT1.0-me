ef015a6c6eeacb3b56c102a6952e4c21639af6a4
zhouruizr <287323540@qq.com>
2017-08-30 18:12:49 +0800

    123

---
+++ IOT1.0/Controllers/Enroll/EnrollProtocolController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOT1._0.Controllers.Enroll
+{
+    public partial class EnrollController : Controller
+    {
+        //
+        // GET: /EnrollProtocol/
+
+        public ActionResult EnrollProtocol()
+        {
+            return View();
+        }
+
+    }
+}
