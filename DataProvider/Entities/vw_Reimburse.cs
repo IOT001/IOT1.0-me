d720d3ba8fc86ad340cd28960d9dcd50aa923a3d
IOT001 <msx8852@126.com>
2017-08-31 17:19:50 +0800

    暂时做完报销查询
    暂时做完报销查询

---
+++ DataProvider/Entities/vw_Reimburse.cs
@@ -0,0 +1,138 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Entities
+{
+
+    //报销表
+    public class vw_Reimburse
+	{
+	
+      	/// <summary>
+		/// ID
+        /// </summary>
+        public   string ID
+        {
+            get; 
+            set; 
+        }        
+		/// <summary>
+		/// 教师姓名
+        /// </summary>
+        public string Teachername
+        {
+            get; 
+            set; 
+        }        
+		/// <summary>
+        /// Reimburse表老师ID
+        /// </summary>
+        public string TeacherID
+        {
+            get; 
+            set; 
+        }
+        /// <summary>
+        /// 状态ID
+        /// </summary>
+        public string StateID
+        {
+            get;
+            set;
+        }        
+		/// <summary>
+        /// 状态名称
+        /// </summary>
+        public string StateIDname
+        {
+            get; 
+            set; 
+        }        
+		/// <summary>
+		/// 申请用途
+        /// </summary>
+        public string ApplyRemark
+        {
+            get; 
+            set; 
+        }        
+	 
+		/// <summary>
+		/// CreateTime
+        /// </summary>
+        public   DateTime? CreateTime
+        {
+            get; 
+            set; 
+        }        
+		/// <summary>
+		/// CreatorId
+        /// </summary>
+        public   string CreatorId
+        {
+            get; 
+            set; 
+        }
+
+        /// <summary>
+        /// 审核时间
+        /// </summary>
+        public Nullable<DateTime> AuditingTime
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        ///  审核人ID
+        /// </summary>
+        public string AuditingID
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 审核人姓名
+        /// </summary>
+        public string AuditingIDname
+        {
+            get;
+            set;
+        }        
+           	/// <summary>
+        /// 审核备注
+        /// </summary>
+        public string AuditingRemark
+        {
+            get; 
+            set; 
+        }
+
+
+        /// <summary>
+        /// 申请退款的金额
+        /// </summary>
+        public decimal ? ApplyPrice
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 财务支付，财务审核时，运行输入比申请金额小的值
+        /// </summary>
+        public decimal? CheckPrice
+        {
+            get;
+            set;
+        }   
+		   
+
+        
+
+	}
+
+    
+}
---
+++ DataProvider/Models/ReimbursetListViewModel.cs
@@ -0,0 +1,80 @@
+﻿using DataProvider.Entities;
+using DataProvider.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace DataProvider.Models
+{
+    public class ReimbursetListViewModel
+    {
+
+        /// <summary>
+        /// 页面查询模型
+        /// </summary>
+        public ReimbursetListSearchModel search = new ReimbursetListSearchModel();
+        /// <summary>
+        /// 页面的列表数据tudents
+        /// </summary>
+        public PagedList<vw_Reimburse> Reimbursetlist { get; set; }
+
+        /// <summary>
+        ///状态下拉框  
+        /// </summary>
+        public List<SelectListItem> StateIDIL { get; set; }
+
+
+        /// <summary>
+        ///老师下拉框  
+        /// </summary>
+        public List<SelectListItem> TeacherIDIL { get; set; }
+
+
+
+    }
+
+    /// <summary>
+    /// 按钮查询模型对象
+    /// </summary>
+    public class ReimbursetListSearchModel : CommonPageEntity
+    {
+        /// <summary>
+        ///老师姓名
+        /// </summary>
+        public string Name { set; get; }
+
+        
+        /// <summary>
+        /// 创建时间开始
+        /// </summary>
+        public string CreateTime_start
+        { set; get; }
+         
+         
+        /// <summary>
+        /// 创建时间结束
+        /// </summary>
+        public string CreateTime_end
+        { set; get; }
+
+
+        /// <summary>
+        /// 状态下拉框按钮的选中值
+        /// </summary>
+        public string StateID { set; get; }
+
+        /// <summary>
+        /// 状态下拉框按钮的选中值
+        /// </summary>
+        public string TeacherID { set; get; }
+
+    }
+
+}
+
+
+
