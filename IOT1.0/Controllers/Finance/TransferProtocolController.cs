eb1df289e364ab2594fd2b347e36e4caa4fc3f84
IOT001 <msx8852@126.com>
2017-09-08 09:56:41 +0800

    好吧。完成了
    好吧。完成了

--- IOT1.0/Controllers/Finance/TransferProtocolController.cs
+++ IOT1.0/Controllers/Finance/TransferProtocolController.cs
@@ -17,13 +17,13 @@ namespace IOT1._0.Controllers.Finance
         public ActionResult TransferProtocol(int id)
         {
             TransferProtocolViewModel model = new TransferProtocolViewModel();
-            int ID = id; 
-            int typeid = int.Parse(Request["typeid"].ToString());//获取类型，1入学协议，2转让协议
+            int ID = id;
+            int typeid = int.Parse(Request["typeid"].ToString());//获取类型，1入学协议，2甲方转让协议，3乙方转让
             vw_Transfer si = TransferData.GetIntKeySign(ID, typeid);
             model.ID = ID;
             model.si = si;
-            model.siB_ImageData =si==null?null:si.siB_ImageData;
-            model.ImageData = si == null ? null : si.ImageData;
+            model.siB_ImageData =si==null?null:si.siB_ImageData;//乙方的判断值
+            model.ImageData = si == null ? null : si.ImageData;//甲方的判断值
             return View(model);
         }
 
