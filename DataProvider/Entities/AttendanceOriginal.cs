4b5dd09b3149b906b403f3b522072fce7fbe3718
zhouruizr <287323540@qq.com>
2017-09-20 09:56:04 +0800

    123

---
+++ DataProvider/Entities/AttendanceOriginal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Entities
+{
+   public class AttendanceOriginal
+    {
+        public AttendanceOriginal()
+        {
+        }
+        /// <summary>
+        /// UserID
+        /// </summary>				
+        public string UserID { get; set; }
+        /// <summary>
+        /// username
+        /// </summary>				
+        public string username { get; set; }
+        /// <summary>
+        /// iMachineNumber
+        /// </summary>				
+        public string iMachineNumber { get; set; }
+        /// <summary>
+        /// InputDate
+        /// </summary>				
+        public DateTime InputDate { get; set; }
+        /// <summary>
+        /// workDates
+        /// </summary>				
+        public DateTime workDates { get; set; }
+        /// <summary>
+        /// idwVerifyMode
+        /// </summary>				
+        public string idwVerifyMode { get; set; }
+        /// <summary>
+        /// 系统识别的结果，有效，无效，重复
+        /// </summary>				
+        public string Recognise { get; set; }
+        /// <summary>
+        /// 识别的时间
+        /// </summary>				
+        public DateTime RecogniseTime { get; set; }
+        /// <summary>
+        /// Classid
+        /// </summary>				
+        public string Classid { get; set; }
+        /// <summary>
+        /// 识别到的班级索引号
+        /// </summary>				
+        public int ClassIndex { get; set; }
+    }
+}
