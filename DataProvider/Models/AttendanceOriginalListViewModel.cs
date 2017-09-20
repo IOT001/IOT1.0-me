fe1466eec133fb09923e1e00153dfa49be9770a4
IOT001 <msx8852@126.com>
2017-09-20 11:50:37 +0800

    完成了考勤识别
    完成了考勤识别

---
+++ DataProvider/Models/AttendanceOriginalListViewModel.cs
@@ -0,0 +1,71 @@
+﻿using DataProvider.Entities;
+using DataProvider.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace DataProvider.Models
+{
+    public class AttendanceOriginalListViewModel
+    {
+
+        /// <summary>
+        /// 页面查询模型
+        /// </summary>
+        public AttendanceOriginalListSearchModel search = new AttendanceOriginalListSearchModel();
+        /// <summary>
+        /// 页面的列表数据AttendanceOriginal
+        /// </summary>
+        public PagedList<vw_AttendanceOriginal> AttendanceOriginallist { get; set; }
+
+  
+
+    }
+
+    /// <summary>
+    /// 按钮查询模型对象
+    /// </summary>
+    public class AttendanceOriginalListSearchModel : CommonPageEntity
+    {
+        /// <summary>
+        ///姓名
+        /// </summary>
+        public string username { set; get; }
+
+        /// <summary>
+        ///学号
+        /// </summary>
+        public string UserID { set; get; }
+
+           /// <summary>
+        ///班级
+        /// </summary>
+        public string ClassName { set; get; }
+
+
+        
+        /// <summary>
+        ///  导入时间开始
+        /// </summary>
+        public string InputDate_start
+        { set; get; }
+
+
+
+        /// <summary>
+        /// 导入时间结束
+        /// </summary>
+        public string InputDate_end
+        { set; get; }
+
+         
+    }
+
+}
+
+
+
--- IOT1.0/Controllers/Attendance/AttendanceOriginalController.cs
+++ IOT1.0/Controllers/Attendance/AttendanceOriginalController.cs
@@ -1,20 +1,43 @@
-﻿using System;
+﻿using DataProvider.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataProvider.Data;
+using DataProvider.Entities;
+using DataProvider;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using IOT1._0.Models;
+
 
 namespace IOT1._0.Controllers.Attendance
 {
     public partial class AttendanceController : Controller
     {
-        //
-        // GET: /AttendanceOriginal/
-
-        public ActionResult AttendanceOriginal()
+ 
+        
+        /// 列表查询
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        public ActionResult AttendanceOriginal(AttendanceOriginalListSearchModel search)
         {
-            return View();
+
+            AttendanceOriginalListViewModel model = new AttendanceOriginalListViewModel();//页面模型
+            model.search = search;//页面的搜索模型
+            model.search.PageSize = 15;//每页显示
+            model.search.CurrentPage = Convert.ToInt32(Request["pageindex"]) <= 0 ? 1 : Convert.ToInt32(Request["pageindex"]);//当前页
+             
+
+            model.AttendanceOriginallist = AttendanceOriginalData.GetAttendanceOriginalDataList(search);//填充页面模型数据
+            return View(model);//返回页面模型
+
+
         }
+
+
 
     }
 }
