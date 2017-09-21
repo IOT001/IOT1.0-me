fc46c21854b3c7ebc7599bd2e74c164bcf6abcce
zhouruizr <287323540@qq.com>
2017-09-21 18:13:36 +0800

    123

--- DataProvider/Data/AttendanceOriginalData.cs
+++ DataProvider/Data/AttendanceOriginalData.cs
@@ -37,7 +37,6 @@ namespace DataProvider.Data
                sb.AppendFormat(" and InputDate > = '{0}' ", search.InputDate_start);
            if (!string.IsNullOrWhiteSpace(search.InputDate_end))//结束时间
                sb.AppendFormat(" and InputDate <= '{0}' ", search.InputDate_end);
-
            where = sb.ToString();
            int allcount = 0;
            var list = CommonPage<vw_AttendanceOriginal>.GetPageList(
@@ -45,6 +44,94 @@ namespace DataProvider.Data
    orderby: orderby, pageindex: search.CurrentPage, pagesize: search.PageSize, connect: DBKeys.PRX);
            return new PagedList<vw_AttendanceOriginal>(list, search.CurrentPage, search.PageSize, allcount);
        }
-       
+        /// <summary>
+        /// 处理原始考勤记录
+        /// </summary>
+        public static void DealAttendanceOriginal(string operatorid)
+        {
+
+            string strsql = "select * from AttendanceOriginal where RecogniseTime is null";
+            List<AttendanceOriginal> aolist = MsSqlMapperHepler.SqlWithParams<AttendanceOriginal>(strsql, null, DBKeys.PRX);//获取所有未识别的数据
+            DBRepository db = new DBRepository(DBKeys.PRX);
+            try
+            {
+                db.BeginTransaction();
+                foreach (AttendanceOriginal ao in aolist)//循环处理
+                {
+                    //智能匹配到这个学员报名过的，时间最接近的班
+                    StringBuilder str = new StringBuilder();
+                    str.AppendFormat(@"SELECT TOP 1 * from ClassList 
+                                    WHERE ABS(DATEDIFF(Hour,ClassDate,'{0}')) < 4 
+                                    AND ClassID IN (SELECT ClassID FROM Enroll WHERE StudentID = {1}) 
+                                    ORDER BY ABS(DATEDIFF(Hour,ClassDate,GETDATE()))", ao.workDates, ao.UserID);
+                    ClassList cl = MsSqlMapperHepler.SqlWithParamsSingle<ClassList>(str.ToString(), null, DBKeys.PRX);//找到唯一班次
+                    if (cl != null)//打卡正确，找到班次
+                    {
+                        //是否之前有考勤，有过考勤记录则不处理
+                        string strar = "select top 1 * from AttendanceRecord where ClassID = '" + cl.ClassID + "' and ClassIndex = " + cl.ClassIndex + " and StudentID = '" + ao.UserID + "'";
+                        AttendanceRecord ar = MsSqlMapperHepler.SqlWithParamsSingle<AttendanceRecord>(strar, null, DBKeys.PRX);
+                        if (ar == null)//不存在就新增
+                        {
+                            ar = new AttendanceRecord();
+                            ar.StudentID = ao.UserID;
+                            ar.ClassID = cl.ClassID;
+                            ar.ClassIndex = ar.ClassIndex;
+                            ar.ClockTime = ao.workDates;
+                            ar.AttendanceTypeID = 2;//正常
+                            ar.AttendanceWayID = 2;//设备考勤
+                            ar.CreateTime = DateTime.Now;
+                            ar.CreatorId = operatorid;
+                            db.Insert(ar);
+
+                            string stren = "select * from Enroll where StudentID = '" + ao.UserID + "' and ClassID = '" + cl.ClassID + "'";
+                            Enroll en = MsSqlMapperHepler.SqlWithParamsSingle<Enroll>(stren, null, DBKeys.PRX);// 找到报名记录
+                            if (en == null)
+                            {
+                                throw new Exception("没有报名记录");
+                            }
+                            else//扣掉学时
+                            {
+                                en.UsedHour = en.UsedHour + 1;
+                                en.UpdateTime = DateTime.Now;
+                                en.UpdatorId = operatorid;
+                                db.Update(en);
+                            }
+                        }
+                        else
+                        {
+                            if (ar.ClockTime == null)//没考勤过
+                            {
+                                ar.ClockTime = ao.workDates;
+                                ar.AttendanceTypeID = 2;//正常
+                                ar.AttendanceWayID = 2;//设备考勤
+                                ar.UpdateTime = DateTime.Now;
+                                ar.UpdatorId = operatorid;
+                                db.Update(ar);
+
+                                string stren = "select * from Enroll where StudentID = '" + ao.UserID + "' and ClassID = '" + cl.ClassID + "'";
+                                Enroll en = MsSqlMapperHepler.SqlWithParamsSingle<Enroll>(stren, null, DBKeys.PRX);// 找到报名记录
+                                if (en == null)
+                                {
+                                    throw new Exception("没有报名记录");
+                                }
+                                else//扣掉学时
+                                {
+                                    en.UsedHour = en.UsedHour + 1;
+                                    en.UpdateTime = DateTime.Now;
+                                    en.UpdatorId = operatorid;
+                                    db.Update(en);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                db.Rollback();
+                db.Dispose();
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
