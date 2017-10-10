f523bd2ae3b74865f66830711f55868821b63798
IOT001 <msx8852@126.com>
2017-10-10 12:19:51 +0800

    完成账号修改功能
    完成账号修改功能

---
+++ DataProvider/Data/Accounts_UpdateData.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using DataProvider.Models;
+using DataProvider.Paging;
+using DataProvider.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Data
+{
+    public class Accounts_UpdateData
+    {
+              /// <summary>
+       /// 跳转查询账号信息
+       /// </summary>
+       /// <param name="apid"></param>
+       /// <returns></returns>
+        public static List<SYS_Account> GetAccounts_Update(int ACC_Id)
+        {
+            String sql = "select * from SYS_Account where ACC_Id = @ACC_Id   ";
+            var dynamic = new DynamicParameters();
+            dynamic.Add("@ACC_Id", ACC_Id);
+            return MsSqlMapperHepler.SqlWithParams<SYS_Account>(sql, dynamic, DBKeys.PRX);  
+        }
+
+          
+        /// <summary>
+        /// 获取单条数据
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public static SYS_Account GetSYS_AccountByID(int ID)
+        {
+            return MsSqlMapperHepler.GetOne<SYS_Account>(ID, DBKeys.PRX);
+        }
+      
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="btn"></param>
+        /// <returns></returns>
+        public static bool UpdateSYS_Account(SYS_Account sys)
+        {
+            SYS_Account sysa = Accounts_UpdateData.GetSYS_AccountByID(sys.ACC_Id);//获取对象
+            Cloner<SYS_Account, SYS_Account>.CopyTo(sys, sysa);//代码克隆，把前台或者的值也就是变更内容复制到目标对象，不做变更的数据不变
+            return MsSqlMapperHepler.Update(sysa, DBKeys.PRX);
+
+        }
+
+
+
+    }
+
+}
---
+++ DataProvider/Models/Accounts_UpdateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DataProvider.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Models
+{
+    public class Accounts_UpdateViewModel
+    {
+        /// <summary>
+        /// 账号记录
+        /// </summary>
+        public List<SYS_Account> SYS_AccountList { get; set; }
+        /// <summary>
+        /// SYS_Account_ID
+        /// </summary>
+        public int ACC_id { set; get; }
+        
+    }
+}
