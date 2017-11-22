59779bb2fcdf40aca44bfbed580408eee29203d6
zhouruizr <287323540@qq.com>
2017-11-22 13:41:04 +0800

    123

---
+++ IOT1.0/Controllers/Enroll/RefundInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOT1._0.Controllers.Enroll
+{
+    public partial class EnrollController : Controller
+    {
+        //
+        // GET: /RefundInfo/
+
+        public ActionResult RefundInfo()
+        {
+            return View();
+        }
+
+    }
+}
