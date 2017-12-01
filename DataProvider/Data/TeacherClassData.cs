1d73342bee3e7e8876d7644e710623a026fc722e
zhouruizr <287323540@qq.com>
2017-12-01 17:44:46 +0800

    123

---
+++ DataProvider/Data/TeacherClassData.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DataProvider.Entities;
+using DataProvider.Models;
+using DataProvider.Paging;
+using DataProvider.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Data
+{
+   public class TeacherClassData
+    {
+        /// <summary>
+        /// 分页获取教师上课表
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+       public static PagedList<vw_ClassAttendanceList> GetAttendanceRecordList(TeacherClassListSearch search)
+        {
+            string table = string.Empty, fields = string.Empty, orderby = string.Empty, where = string.Empty;//定义结构
+            fields = @"  * ";//输出字段
+            table = @" vw_ClassAttendanceList ";//表或者视图
+            orderby = "ID";//排序信息
+            StringBuilder sb = new StringBuilder();//构建where条件
+            sb.Append(" 1=1 ");
+
+
+            if (!string.IsNullOrWhiteSpace(search.timeStart))//开班时间
+                sb.AppendFormat(" and ClassDate > = '{0}' ", search.timeStart);
+            if (!string.IsNullOrWhiteSpace(search.timeEnd))//结束时间
+                sb.AppendFormat(" and ClassDate <= '{0}' ", search.timeEnd);
+
+
+            if (!string.IsNullOrWhiteSpace(search.teacherID))//vw_AttendanceRecord ID
+                sb.AppendFormat(" and  TeacherID ='{0}' ", search.teacherID);
+            where = sb.ToString();
+            int allcount = 0;
+            var list = CommonPage<vw_ClassAttendanceList>.GetPageList(
+    out allcount, table, fields: fields, where: where.Trim(),
+    orderby: orderby, pageindex: search.CurrentPage, pagesize: search.PageSize, connect: DBKeys.PRX);
+            return new PagedList<vw_ClassAttendanceList>(list, search.CurrentPage, search.PageSize, allcount);
+        }
+
+    }
+}
---
+++ IOT1.0/Controllers/WeiXin/TeacherClassController.cs
@@ -0,0 +1,37 @@
+﻿using DataProvider;
+using DataProvider.Data;
+using DataProvider.Entities;
+using DataProvider.Models;
+using IOT1._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IOT1._0.Controllers.WeiXin
+{
+    public partial class WeixinController : Controller
+    {
+        //
+        // GET: /TeacherClass/
+        /// <summary>
+        /// 教师对应的上课记录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TeacherClassList(TeacherClassListSearch search)
+        {
+            WX_TeacherClassListViewModel model = new WX_TeacherClassListViewModel();
+            model.search = search;
+            model.search.PageSize = 9999;//每页显示1000条数据
+            model.search.CurrentPage = model.search.CurrentPage <= 0 ? 1 : model.search.CurrentPage;//获取当前页
+            Teachers s = TeacherData.GetTeachByID(UserSessionWX.userid);//获取当前教师
+            if (s != null)
+            {
+                model.TeacherClassList = TeacherClassData.GetAttendanceRecordList(search);
+            }
+            return View();
+        }
+
+    }
+}
