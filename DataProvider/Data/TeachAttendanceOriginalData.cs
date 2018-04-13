91a817688f1655a6f8ceb0ed09d1f33d55608b61
IOT001 <msx8852@126.com>
2018-04-13 16:23:36 +0800

    教师打卡考勤
    教师打卡考勤

---
+++ DataProvider/Data/TeachAttendanceOriginalData.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DataProvider.Entities;
+using DataProvider.Models;
+using DataProvider.Paging;
+using DataProvider.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.Data
+{
+    public class TeachAttendanceOriginalData
+    {
+       /// <summary>
+        /// 分页获取考勤识别列表
+       /// </summary>
+       /// <param name="search"></param>
+       /// <returns></returns>
+        public static PagedList<vw_AttendanceOriginal> GetAttendanceOriginalDataList(TeachAttendanceOriginalListSearchModel search)
+       {
+           string table = string.Empty, fields = string.Empty, orderby = string.Empty, where = string.Empty;//定义结构
+           fields = @"  * ";//输出字段
+           table = @" vw_AttendanceOriginal ";//表或者视图
+           orderby = "InputDate desc";//排序信息
+           StringBuilder sb = new StringBuilder();//构建where条件
+           sb.Append(" 1=1 AND UserID <=5");
+           if (!string.IsNullOrWhiteSpace(search.username))//姓名
+               sb.AppendFormat(" and username like '%{0}%' ", search.username);  
+
+           if (!string.IsNullOrWhiteSpace(search.InputDate_start))//开班时间
+               sb.AppendFormat(" and InputDate > = '{0}' ", search.InputDate_start);
+           if (!string.IsNullOrWhiteSpace(search.InputDate_end))//结束时间
+               sb.AppendFormat(" and InputDate <= '{0}' ", search.InputDate_end);
+           where = sb.ToString();
+           int allcount = 0;
+           var list = CommonPage<vw_AttendanceOriginal>.GetPageList(
+   out allcount, table, fields: fields, where: where.Trim(),
+   orderby: orderby, pageindex: search.CurrentPage, pagesize: search.PageSize, connect: DBKeys.PRX);
+           return new PagedList<vw_AttendanceOriginal>(list, search.CurrentPage, search.PageSize, allcount);
+       }
+       
+
+
+
+    }
+}
---
+++ DataProvider/Models/TeachAttendanceOriginalListViewModel.cs
@@ -0,0 +1,63 @@
+﻿using DataProvider.Entities;
+using DataProvider.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace DataProvider.Models
+{
+    public class TeachAttendanceOriginalListViewModel
+    {
+
+        /// <summary>
+        /// 页面查询模型
+        /// </summary>
+        public TeachAttendanceOriginalListSearchModel search = new TeachAttendanceOriginalListSearchModel();
+        /// <summary>
+        /// 页面的列表数据AttendanceOriginal
+        /// </summary>
+        public PagedList<vw_AttendanceOriginal> AttendanceOriginallist { get; set; }
+
+  
+
+    }
+
+    /// <summary>
+    /// 按钮查询模型对象
+    /// </summary>
+    public class TeachAttendanceOriginalListSearchModel : CommonPageEntity
+    {
+        /// <summary>
+        ///姓名
+        /// </summary>
+        public string username { set; get; }
+
+    
+
+
+        
+        /// <summary>
+        ///  导入时间开始
+        /// </summary>
+        public string InputDate_start
+        { set; get; }
+
+
+
+        /// <summary>
+        /// 导入时间结束
+        /// </summary>
+        public string InputDate_end
+        { set; get; }
+
+         
+    }
+
+}
+
+
+
